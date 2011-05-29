be802ddca0b5319ce510c9b0f3871cb96c67c79d
Loic Nageleisen <loic.nageleisen@gmail.com>
2011-05-29 18:29:21 +0200

    simpler exclusion

--- scripts/make_links.sh
+++ scripts/make_links.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
 for dotfile in *; do
-    if [ "${dotfile}" != "scripts" ]; then
-        echo "linking ${dotfile}"
-        ln -sf "$(pwd)/${dotfile}" "$HOME/.${dotfile}"
-    fi
+    case "${dotfile}" in
+        scripts|Makefile)
+            # NOOP
+            ;;
+        *)
+            # link the file with a leading dot
+            echo "linking ${dotfile}"
+            ln -sf "$(pwd)/${dotfile}" "$HOME/.${dotfile}"
+            ;;
+    esac
 done
 
