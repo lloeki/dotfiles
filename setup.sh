fefe99ae9f4345783aab326f115fed34ae48702b
Loic Nageleisen <loic.nageleisen@gmail.com>
2021-12-01 00:36:33 +0100

    Clean setup

--- setup.sh
+++ setup.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
-[ $(pwd) = "$HOME/.dotfiles" ] || ln -sfn "$(pwd)" "$HOME/.dotfiles"
-
 for file in home/*; do
     dotfile="$(basename "$file")"
     case "${dotfile}" in
         Makefile|*.md|LICENSE|setup.sh)
-            # NOOP
+            : # NOOP
             ;;
         *)
-            # link the file with a leading dot
-            echo "linking ${dotfile}"
-            ln -sf "$(pwd)/${file}" "$HOME/.${dotfile}"
+            if [ -d "${dotfile}" ]; then
+                :
+            else
+              # link the file with a leading dot
+              echo "linking ${dotfile}"
+              ln -sf "${PWD}/${file}" "$HOME/.${dotfile}"
+            fi
             ;;
     esac
 done
